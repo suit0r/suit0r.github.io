8ebefdbccfbddd7932ae37ecfcc3018a8adaee9c
Irakli <irakli@live.ca>
2018-10-27 18:42:47 -0400

    service worker

--- precache-manifest.c9e8a1fd2141b935b12b3bc4313263d4.js
+++ precache-manifest.c9e8a1fd2141b935b12b3bc4313263d4.js
@@ -1,58 +1,58 @@
 self.__precacheManifest = [
   {
     "revision": "baa1e2496e8c71f636587c32f4016ca4",
-    "url": "/fonts/fa-solid-900.baa1e249.eot"
+    "url": "./fonts/fa-solid-900.baa1e249.eot"
   },
   {
     "revision": "7c3919fe8f5ce138dbd887ca9dee9728",
-    "url": "/img/banner.jpg"
+    "url": "./img/banner.jpg"
   },
   {
     "revision": "dbebf5db343bfc92c68b004ad3b88355",
-    "url": "/img/bg.jpg"
+    "url": "./img/bg.jpg"
   },
   {
     "revision": "bd3736d5cc086c8522ec",
-    "url": "/js/chunk-vendors.9d6abeec.js"
+    "url": "./js/chunk-vendors.9d6abeec.js"
   },
   {
     "revision": "04281249a5eea3c4dc6b0b9379c194f0",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "3f759796d3d16b8434841d61cd2c007a",
-    "url": "/img/fa-solid-900.3f759796.svg"
+    "url": "./img/fa-solid-900.3f759796.svg"
   },
   {
     "revision": "ad27dbbb77096fb881bc",
-    "url": "/js/app.e6917cb2.js"
+    "url": "./js/app.e6917cb2.js"
   },
   {
     "revision": "dd70dc5b1cfc3392b3acfe0376f2218f",
-    "url": "/img/banner-mobile.jpg"
+    "url": "./img/banner-mobile.jpg"
   },
   {
     "revision": "fd531d212b567d6049f400165473589f",
-    "url": "/fonts/fa-solid-900.fd531d21.woff2"
+    "url": "./fonts/fa-solid-900.fd531d21.woff2"
   },
   {
     "revision": "e143b57de78138e6d5963908afa7e393",
-    "url": "/fonts/fa-solid-900.e143b57d.ttf"
+    "url": "./fonts/fa-solid-900.e143b57d.ttf"
   },
   {
     "revision": "09bc9e8c7d6dfdace635ea073974db13",
-    "url": "/fonts/fa-solid-900.09bc9e8c.woff"
+    "url": "./fonts/fa-solid-900.09bc9e8c.woff"
   },
   {
     "revision": "bd3736d5cc086c8522ec",
-    "url": "/css/chunk-vendors.3043cf72.css"
+    "url": "./css/chunk-vendors.3043cf72.css"
   },
   {
     "revision": "ad27dbbb77096fb881bc",
-    "url": "/css/app.7566a942.css"
+    "url": "./css/app.7566a942.css"
   },
   {
     "revision": "afadd5ebc15898a3f668da6a7c033448",
-    "url": "/animation.css"
+    "url": "./animation.css"
   }
 ];
